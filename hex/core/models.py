9c43a2fcc0d36c12a61b52647d514f2cf025883b
Olaf Ociepa <ociepa.olaf@gmail.com>
2022-05-30 14:33:36 +0200

    creating user

--- hex/core/models.py
+++ hex/core/models.py
@@ -1,3 +1,72 @@
+"""
+Database models.
+"""
 from django.db import models
+import os
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+# from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    BASIC = 'bp'
+    PREMIUM = 'pp'
+    ENTERPRISE = 'ep'
+    PLAN_CHOICE = [
+        (BASIC, 'BASIC PLAN'),
+        (PREMIUM, 'PREMIUM PLAN'),
+        (ENTERPRISE, 'ENTERPRISE PLAN'),
+    ]
+    perm = models.CharField(max_length=2, choices=PLAN_CHOICE, default=BASIC)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+# class UserPerms(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     BASIC = 'bp'
+#     PREMIUM = 'pp'
+#     ENTERPRISE = 'ep'
+#     PLAN_CHOICE = [
+#         (BASIC, 'BASIC PLAN'),
+#         (PREMIUM, 'PREMIUM PLAN'),
+#         (ENTERPRISE, 'ENTERPRISE PLAN'),
+#     ]
+#     perm = models.CharField(max_length=2, choices=PLAN_CHOICE, default=BASIC)
